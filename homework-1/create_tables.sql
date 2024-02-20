11d396d21c45cd87fac895bd9bab7acb1b073999
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-02-20 23:33:07 +0300

    - заполнен файл-код.sql для создания таблиц - реализована функция для подключения к БД

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+-- create employees table
+CREATE TABLE employees (
+	employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes varchar(500)
+);
+
+-- create customers table
+CREATE TABLE customers (
+	customer_id int PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+-- create orders table
+CREATE TABLE orders (
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) NOT NULL,
+	employee_id varchar(100),
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,16 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+from typing import Any
+
+
+# connect to db
+def connect_to_db() -> Any:
+    return psycopg2.connect(
+        database='north',
+        host='localhost',
+        password='2277',
+        user='postgres')
+
+
+
