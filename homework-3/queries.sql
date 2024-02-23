b963c1fdb901ecebbbe231c6010773eb0d9c949f
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-02-23 21:37:29 +0300

    - Add #3 sql query

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -19,7 +19,11 @@ WHERE discontinued <> 1 AND units_in_stock < 25 AND category_id IN (4, 2)
 ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+LEFT JOIN orders USING(customer_id)
+WHERE orders.order_id IS NULL
+ORDER BY customer_id
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
